05f753bcc1e7a25addab4351ee6ffb0bf56e493a
grace.fidele <gngrace10@gmail.com>
2023-06-05 20:57:30 +0200

    rebasing

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode> 
+  // {/* </React.StrictMode> */}
 )
--- src/pages/Sample.tsx
+++ src/pages/Sample.tsx
@@ -1,27 +1,26 @@
-import { ReactNode, useState } from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import "./sample.css"
-import Sidebar from '../components/Sidebar/side'
+import { ReactNode, useState } from "react";
+import Navbar from "../components/Navbar/Navbar";
+import "./sample.css";
+import Sidebar from "../components/Sidebar/side";
 
-interface Props{
-    setActivateSideBar:Function,
-    activateSideBar: boolean,
-    children: ReactNode
+interface Props {
+  setActivateSideBar: Function;
+  activateSideBar: boolean;
+  children: ReactNode;
 }
-export default function Sample ({children}:any) {
-const [activateSideBar,setActivateSideBar] = useState(true)
+export default function Sample({ children }: any) {
+  const [activateSideBar, setActivateSideBar] = useState(true);
 
-
-
-    return (
-        <div className='overall-container'>
-            <Navbar setActivateSideBar={setActivateSideBar} activateSideBar={activateSideBar}/>
-            <div className='section-container row'>
-            <Sidebar activateSideBar={activateSideBar}/>
-            {children}
-            </div>
-           
-
-        </div>
-    )
+  return (
+    <div className="overall-container">
+      <Navbar
+        setActivateSideBar={setActivateSideBar}
+        activateSideBar={activateSideBar}
+      />
+      <div className="section-container row">
+        <Sidebar activateSideBar={activateSideBar} />
+        {children}
+      </div>
+    </div>
+  );
 }
