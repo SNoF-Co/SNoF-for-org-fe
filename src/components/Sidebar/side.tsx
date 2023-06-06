7fdf501a42ba6edac31f4666cc99391ba596b0a5
grace.fidele <gngrace10@gmail.com>
2023-06-06 08:28:01 +0200

    rebasing

--- src/components/Sidebar/side.tsx
+++ src/components/Sidebar/side.tsx
@@ -1,110 +1,92 @@
-import './side.css'
-import { CiClock2 } from 'react-icons/ci'
-import { SiGoogleanalytics } from 'react-icons/si'
-import { MdHomeFilled ,MdOutlineHelpOutline, MdOutlineLogout } from 'react-icons/md'
-import { VscFeedback } from 'react-icons/vsc'
-import { IoMdSettings } from 'react-icons/io'
-import { Link } from 'react-router-dom'
+import "./side.css";
+import { CiClock2 } from "react-icons/ci";
+import { SiGoogleanalytics } from "react-icons/si";
+import {
+  MdHomeFilled,
+  MdOutlineHelpOutline,
+  MdOutlineLogout,
+} from "react-icons/md";
+import { VscFeedback } from "react-icons/vsc";
+import { IoMdSettings } from "react-icons/io";
+import { Link, NavLink } from "react-router-dom";
 
 interface Props {
-    activateSideBar: boolean
+  activateSideBar: boolean;
 }
-const Sidebar:React.FC<Props>=({activateSideBar})=> {
-    
-    return (
-        <>
-            { activateSideBar && <div className='container'>
+const Sidebar: React.FC<Props> = ({ activateSideBar }) => {
+  return (
+    <>
+      {activateSideBar && (
+        <div className="container">
+          <div className="container--upper">
+            <NavLink to={"/dashboard#f"}>
+              <div className="link-container">
+                <div className="link-item">
+                  <MdHomeFilled />
+                </div>
+                <div className="link-name">Dashboard</div>
+              </div>
+            </NavLink>
+            <NavLink to={"/schedule"}>
+              <div className="link-container">
+                <div className="link-item">
+                  <CiClock2 />
+                </div>
+                <div className="link-name">Schedules</div>
+              </div>
+            </NavLink>
 
-            <div className="container--upper">
-                <Link to={"/dashboard#f"}>
-                    <div className="link-container active">
-                        <div className="link-item">
-                            <MdHomeFilled />
-                        </div>
-                        <div className="link-name">
-                            Dashboard
-                        </div>
-                    </div>
-                </Link>
-                <Link to={"/schedule"}>
-                    <div className="link-container">
-                        <div className="link-item">
-                            <CiClock2 />
-                        </div>
-                        <div className="link-name">
-                            Schedules
-                        </div>
-                    </div>
-                </Link>
-                
-                <Link to={"/analytics"}>
-                    <div className="link-container">
-                        <div className="link-item">
-                            <SiGoogleanalytics />
-                        </div>
-                        <div className="link-name">
-                            Analytics
-                        </div>
-                    </div>
-                </Link>
-                
-                <Link to={"/feedback"}>
-                    <div className="link-container">
-                        <div className="link-item">
-                            <VscFeedback />
-                        </div>
-                        <div className="link-name">
-                            Feedback
-                        </div>
-                    </div>
-                </Link>
-               
-            </div>
+            <NavLink to={"/analytics"}>
+              <div className="link-container">
+                <div className="link-item">
+                  <SiGoogleanalytics />
+                </div>
+                <div className="link-name">Analytics</div>
+              </div>
+            </NavLink>
 
-            <div className="container--lower">
-                <Link to={"/help"}>
-                    <div className="link-container">
-                        <div className="link-item">
-                            <MdOutlineHelpOutline />
-                        </div>
-                        <div className="link-name">
-                            Help
-                        </div>
-                    </div>
-                </Link>
-               
-                <Link to={"/settings"}>
-                    <div className="link-container">
-                    
-                        <div className="link-item">
-                            <IoMdSettings />
-                        </div>
-                        <div className="link-name">
-                            Settings
-                        </div>
-                    </div>
-                </Link>
-                
+            <NavLink to={"/feedback"}>
+              <div className="link-container">
+                <div className="link-item">
+                  <VscFeedback />
+                </div>
+                <div className="link-name">Feedback</div>
+              </div>
+            </NavLink>
+          </div>
 
-                <Link to={"/logout"}>
-                    <div className="link-container logout">
-                        <div className="link-item">
-                            <MdOutlineLogout />
-                        </div>
-                        <div className="link-name">
-                            Logout
-                        </div>
-                    </div>
-                </Link>
-               
-            </div>
-            </div>}
-            
-        </>
-        
-       
-    )
-}
+          <div className="container--lower">
+            <NavLink to={"/help"}>
+              <div className="link-container">
+                <div className="link-item">
+                  <MdOutlineHelpOutline />
+                </div>
+                <div className="link-name">Help</div>
+              </div>
+            </NavLink>
 
-export default Sidebar
+            <NavLink to={"/settings"}>
+              <div className="link-container">
+                <div className="link-item">
+                  <IoMdSettings />
+                </div>
+                <div className="link-name">Settings</div>
+              </div>
+            </NavLink>
 
+            <NavLink to={"/logout"}>
+              <div className="link-container logout">
+                <div className="link-item">
+                  <MdOutlineLogout />
+                </div>
+                <div className="link-name">Logout</div>
+              </div>
+            </NavLink>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Sidebar;
