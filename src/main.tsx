efd6a82770701e1f36dec9b8a1451ef0eac8ceeb
grace.fidele <gngrace10@gmail.com>
2023-06-06 07:42:21 +0200

    rebasing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import './index.css'
 >>>>>>> ad68fe1 (resolve rebase conflicts)
+=======
+>>>>>>> 75e60c2 (resolve rebase conflicts)
 import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
