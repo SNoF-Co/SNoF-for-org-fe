c0962d669d536e2f31283859761b90afdf9906d5
grace.fidele <gngrace10@gmail.com>
2023-06-06 07:34:24 +0200

    rebasing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+<<<<<<< HEAD
+=======
+import './index.css'
+>>>>>>> ad68fe1 (resolve rebase conflicts)
 import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
     <App />
-  // {/* </React.StrictMode> */}
+    </BrowserRouter>
+  </React.StrictMode>,
 )
